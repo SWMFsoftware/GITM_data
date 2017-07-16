a623b7808e577ef7afc8af1d6952ec7a8a7811db
ridley <>
2017-07-16 15:03:13 +0000

    Fixed a bug on the change directories.

--- data/input/test_earth.sh
+++ data/input/test_earth.sh
@@ -6,8 +6,9 @@
 
 ./Config.pl -g=1,1,50,4
 make
-rm -rf run
+rm -rf run test_*.diff
 
+rm -rf run_1d
 make rundir
 mv run run_1d
 cp srcData/UAM.in.1d run_1d/UAM.in
@@ -19,6 +20,7 @@ cd UA ; pGITM ; cd ..
 cd ..
 ls -l test_1d.diff
 
+rm -rf run_eclipse
 make rundir
 mv run run_eclipse
 cp srcData/UAM.in.eclipse run_eclipse/UAM.in
@@ -36,17 +38,17 @@ ls -l test_eclipse.diff
 
 ./Config.pl -g=9,9,50,4
 make
-rm -rf run
 
+rm -rf run_3d
 make rundir
 mv run run_3d
 cp srcData/UAM.in.3d run_3d/UAM.in
 cd run_3d
 mpirun -np 4 ./GITM.exe
 ../share/Scripts/DiffNum.pl -b -r=1e-5 UA/data/log0000000?.dat UA/DataIn/log0000000?.3d.dat >& ../test_3d.diff
-diff UA/data/run_information.txt UA/DataIn/run_information.txt >> ../test_3d.diff
+diff UA/data/run_information.txt UA/DataIn/run_information.3d.txt >> ../test_3d.diff
 cd UA ; pGITM ; cd ..
-cd data ; idl < ../DataIn/idl_input.3d ; cd ..
-cd ../..
+cd UA/data ; idl < ../DataIn/idl_input.3d ; cd ../..
+cd ..
 ls -l test_3d.diff
 
